a72b4f158bae7e8f0be7a8c9f2357f977dd5f4fe
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2022-04-27 01:39:37 -0400

    Add initial files

---
+++ src/main.cpp
@@ -0,0 +1,355 @@
+#include <Arduino.h>
+#include <AltSoftSerial.h>
+
+// Define functions
+#define sprint(x) Serial.print(x)
+#define sprintln(x) Serial.println(x)
+
+#define START_BYTE 0x1
+#define END_BYTE 0x4
+#define DATA_LENGTH 0x8
+
+#define SENSOR_DATA_ID 0xD7
+#define STOP_MOTORS_ID 0xE0
+#define PARENTAL_OVERRIDE_ID 0xDF
+
+#define MIN_MOTOR_SPEED 0
+#define MAX_MOTOR_SPEED 255
+
+// H-Bridge Pins
+#define EnA 3
+#define EnB 11
+
+// Motor A
+#define In1 4
+#define In2 7
+
+// Motor B
+#define In3 2
+#define In4 12
+
+enum Motor
+{
+    LEFT,
+    RIGHT
+};
+
+typedef struct GoPacket_t
+{
+    int startByte = START_BYTE;
+    int id;
+    bool ack;
+    int dataSign;
+    int sensorX;
+    int sensorY;
+    int endByte = END_BYTE;
+
+} GoPacket;
+
+AltSoftSerial bluetooth;
+
+uint64_t timeNow = 0;
+uint32_t safetyTimer = 200;
+
+bool receivedAck = false;
+bool stoppedMotors = false;
+
+uint32_t state = 0;
+
+int packetId;
+int packetDataSign;
+int packetSensorX;
+int packetSensorY;
+
+// Functions
+
+void setupMotors()
+{
+    pinMode(EnA, OUTPUT);
+    pinMode(EnB, OUTPUT);
+
+    pinMode(In1, OUTPUT);
+    pinMode(In2, OUTPUT);
+    pinMode(In3, OUTPUT);
+    pinMode(In4, OUTPUT);
+}
+
+void setMotorSpeed(Motor motor, int motorSpeed)
+{
+    int motorEnaPin,
+        motorAPin,
+        motorBPin;
+
+    switch (motor)
+    {
+    case LEFT:
+        motorEnaPin = EnA;
+        motorAPin = In1;
+        motorBPin = In2;
+        break;
+
+    case RIGHT:
+        motorEnaPin = EnB;
+        motorAPin = In3;
+        motorBPin = In4;
+        break;
+    }
+
+    // Reverse
+    if (motorSpeed < 0)
+    {
+        digitalWrite(motorAPin, HIGH);
+        digitalWrite(motorBPin, LOW);
+    }
+    // Forward
+    else if (motorSpeed > 0)
+    {
+        digitalWrite(motorAPin, LOW);
+        digitalWrite(motorBPin, HIGH);
+    }
+    // Stop
+    else
+    {
+        digitalWrite(motorAPin, LOW);
+        digitalWrite(motorBPin, LOW);
+    }
+
+    analogWrite(motorEnaPin, (int)constrain(abs(motorSpeed), MIN_MOTOR_SPEED, MAX_MOTOR_SPEED));
+}
+
+void setMotorSpeeds(int leftMotorSpeed, int rightMotorSpeed)
+{
+    setMotorSpeed(LEFT, leftMotorSpeed);
+    setMotorSpeed(RIGHT, rightMotorSpeed);
+}
+
+float scaleSensorX(int v)
+{
+    return .40 * v;
+}
+
+float scaleSensorY(int v)
+{
+    return v * .115;
+}
+
+void safetyIsrMotors()
+{
+    if (millis() > timeNow + safetyTimer)
+    {
+        timeNow = millis();
+
+        if (receivedAck)
+        {
+            stoppedMotors = false;
+        }
+        else if (!stoppedMotors)
+        {
+            // sprintln("Safety: Stopping Motors");
+            setMotorSpeeds(0x0, 0x0);
+            stoppedMotors = true;
+        }
+
+        receivedAck = false;
+    }
+}
+
+void processStopMotors()
+{
+    sprintln("Stopping Motors");
+    setMotorSpeeds(0x0, 0x0);
+}
+
+void processParentalOverride()
+{
+    sprintln("Parental Override");
+}
+
+// TODO: remember to free returned array
+int* getActualSensorVals(int sign, int receivedSensorX, int receivedSensorY)
+{
+    int* actualSensorVals = (int*) malloc(2 * sizeof(int));
+    actualSensorVals[0] = receivedSensorX;
+    actualSensorVals[1] = receivedSensorY;
+
+    switch (sign)
+    {
+    case 0x1:
+        // Sensor X Negative
+        actualSensorVals[0] = -actualSensorVals[0];
+        break;
+    
+    case 0x2:
+        // Sensor Y Negative
+        actualSensorVals[1] = -actualSensorVals[1];
+        break;
+
+    case 0x3:
+        // Sensor X and Y Negative
+        actualSensorVals[0] = -actualSensorVals[0];
+        actualSensorVals[1] = -actualSensorVals[1];
+        break;
+    
+    default:
+        break;
+    }
+
+    return actualSensorVals;
+}
+
+void processSensorData(int sensorSign, int sensorX, int sensorY)
+{
+    int* actualSensorVals = getActualSensorVals(sensorSign, sensorX, sensorY);
+
+    int actualSensorX = actualSensorVals[0];
+    int actualSensorY = actualSensorVals[1];
+
+    free(actualSensorVals);
+
+    int scaledSensorX = (int)floor(scaleSensorX(actualSensorX));
+    int scaledSensorY = (int)floor(scaleSensorY(actualSensorY));
+
+    // Flip turning direction from phone
+    int motor1Speed = scaledSensorX - scaledSensorY;
+    int motor2Speed = scaledSensorX + scaledSensorY;
+
+    // sprintln("M1: " + String(motor1Speed) + 
+    //                " M2: " + String(motor2Speed) + 
+    //                " A: " + String(scaledSensorY));
+
+    setMotorSpeeds(motor1Speed, motor2Speed);
+}
+
+void bluetoothStateMachine()
+{
+    if (bluetooth.available() > 0)
+    {
+        int dataRead = bluetooth.read();
+        Serial.println(dataRead, HEX);
+
+        switch (state)
+        {
+        case 0:
+            if (dataRead == START_BYTE)
+                ++state;
+            else
+                state = 0;
+            break;
+
+        case 1:
+            packetId = dataRead;
+            ++state;
+            break;
+
+        case 2:
+            if (dataRead == 0x1 || dataRead == 0x0)
+            {
+                receivedAck = (bool)dataRead;
+                ++state;
+            }
+            else
+            {
+                state = 0;
+            }
+            break;
+
+        case 3:
+            packetDataSign = dataRead;
+            ++state;
+            break;
+
+        case 4:
+            packetSensorX = dataRead;
+            ++state;
+            break;
+
+        case 5:
+            packetSensorY = dataRead;
+            ++state;
+            break;
+
+        case 6:
+            if (dataRead == END_BYTE)
+            {
+                switch (packetId)
+                {
+                case STOP_MOTORS_ID:
+                    processStopMotors();
+                    break;
+
+                case SENSOR_DATA_ID:
+                    processSensorData(packetDataSign, packetSensorX, packetSensorY);
+                    break;
+
+                case PARENTAL_OVERRIDE_ID:
+                    processParentalOverride();
+                    break;
+                }
+            }
+
+            // Reset state vars
+            state = 0;
+
+            packetId = 0;
+            packetDataSign = 0;
+            packetSensorX = 0;
+            packetSensorY = 0;
+
+            break;
+
+        default:
+            ++state;
+            break;
+        }
+    }
+}
+
+void readSerial()
+{
+    if (Serial.available())
+    {
+        while (Serial.available() > 0)
+        {
+            bluetooth.print(Serial.read());
+        }
+        bluetooth.println();
+    }
+
+    if (bluetooth.available())
+    {
+        while (bluetooth.available() > 0)
+        {
+            Serial.print(bluetooth.read(), HEX);
+        }
+        Serial.println('\n');
+    }
+}
+
+/* ------------------------- */
+
+uint32_t bt_delay = 500;
+
+void setup()
+{
+    Serial.begin(9600);
+    sprintln("Arduino Up");
+
+    while(!Serial);
+
+    delay(500);
+
+    bluetooth.begin(57600);
+
+    // delay(bt_delay);
+    // bluetooth.write("AT");
+
+    delay(bt_delay);
+    bluetooth.write("AT+BAUD7");
+}
+
+void loop()
+{
+    safetyIsrMotors();
+    bluetoothStateMachine();
+    // readSerial();
+}
